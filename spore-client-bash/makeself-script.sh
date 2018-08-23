94d3aad7c1c238eab651d2afebbf869ff7b061e6
Antoinne Viscardi <antoine.viscardi@gmail.com>
2018-08-23 15:05:09 -0400

    deleted makeself-script

--- spore-client-bash/makeself-script.sh
+++
@@ -1,6 +0,0 @@
-[[ -z $1 ]] && echo Error: Must provide path to makeself.sh && exit 1
-
-msg="Spore Service to seed local entorpy source on startup"
-$1 ./ spore-client-service $msg ./install.sh
-rm ../release/* 2> /dev/null
-mv spore-client-service ../release/spore-client-service
